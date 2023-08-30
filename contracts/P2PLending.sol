9c5400de38ce6632b8fba83560dd215a3cde8d9a
nithronium <osmankuzucu5@gmail.com>
2023-08-30 16:08:12 +0300

    addition of network deployment

--- contracts/P2PLending.sol
+++ contracts/P2PLending.sol
@@ -9,51 +9,103 @@ contract P2PLending is TokenImplementer {
   struct Depositor {
     uint256 amount;
     uint256 availableAmount;
+  }
+
+  struct CreditOptions {
+    uint256 maxAmount;
+    uint256 minAmount;
     uint256 interestRate;
-    address belongsTo;
-    uint256 maxPerAddress;
+    uint256 maxDuration;
+    uint256 collateralRate;
+    bool isActive;
   }
 
   struct Borrower {
     uint256 collateralAmount;
+    uint256 availableCollateralAmount;
     uint256 loanAmount;
-    address belongsTo;
-    uint256 availableCollateral;
+    uint256 loanRepaid;
   }
 
   // Array of depositors
-  Depositor[] public depositors;
+  mapping(address => Depositor) public depositors;
+  mapping(address => CreditOptions[]) public creditOptions;
+  mapping(address => Borrower) public borrowers;
+
+  modifier onlyDepositor() {
+    require(depositors[msg.sender].amount > 0, "Not a depositor");
+    _;
+  }
 
   /**
    * @dev Function to deposit tokens into the contract
    * 
    * @param _amount The amount of tokens to deposit
-   * @param _interestRate interest rate in percent
-   * @param _maxPerAddress maximum amount of tokens that can be loaned to a single address
    */
   function depositMoney(
-    uint256 _amount,
-    uint256 _interestRate,
-    uint256 _maxPerAddress
+    uint256 _amount
   ) public {
-
     // Check if the amount is greater than the minimum allowed
     require(_amount >= getMinimumAllowedDeposit(), "Amount is too low");
 
     // Withdraw the tokens from the user
     getTokensFromUser(msg.sender, _amount);
 
-    // Create a new depositor
-    Depositor memory newDepositor = Depositor({
-      amount: _amount,
-      availableAmount: _amount,
-      interestRate: _interestRate,
-      belongsTo: msg.sender,
-      maxPerAddress: _maxPerAddress
-    });
+    // Update depositor balances
+    depositors[msg.sender].amount += _amount;
+    depositors[msg.sender].availableAmount += _amount;
+  }
 
-    // Add the depositor to the array
-    depositors.push(newDepositor);
+  function withdrawMoney(uint256 amount) public onlyDepositor {
+    require(depositors[msg.sender].availableAmount >= amount, "Amount is too high");
+    depositors[msg.sender].availableAmount -= amount;
+    depositors[msg.sender].amount -= amount;
+    token.transfer(msg.sender, amount);
+  }
+
+  function addCreditOption(
+    uint256 _maxAmount,
+    uint256 _minAmount,
+    uint256 _interestRate,
+    uint256 _maxDuration,
+    uint256 _collateralRate
+  ) public onlyDepositor {
+    creditOptions[msg.sender].push(
+      CreditOptions(
+        _maxAmount,
+        _minAmount,
+        _interestRate,
+        _maxDuration,
+        _collateralRate,
+        true
+      )
+    );
+  }
+ 
+  function adjustCreditOption(uint256 index, bool isActive) public onlyDepositor {
+    creditOptions[msg.sender][index].isActive = isActive;
+  }
+
+
+  function depositCollateral(
+    uint256 _amount
+  ) public {
+    // Check if the amount is greater than the minimum allowed
+    require(_amount >= getMinimumAllowedDeposit(), "Amount is too low");
+
+    // Withdraw the tokens from the user
+    getCollateralFromUser(msg.sender, _amount);
+
+    // Update depositor balances
+    borrowers[msg.sender].collateralAmount += _amount;
+    borrowers[msg.sender].availableCollateralAmount += _amount;
+  }
+
+  function withdrawCollateral(uint256 amount) public {
+    require(borrowers[msg.sender].availableCollateralAmount >= amount, "Amount is too high");
+    borrowers[msg.sender].availableCollateralAmount -= amount;
+    borrowers[msg.sender].collateralAmount -= amount;
+    collateral.transfer(msg.sender, amount);
   }
 
 }
